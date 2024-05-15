955a70c0f48b02fa0c470dfecd5b6d6e6f9b3b42
ortima <account.net@.mail.ru>
2024-05-16 00:39:08 +0300

    chadcn dep

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,3 +1,33 @@
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { Github, Send } from "lucide-react";
+import { Navbar } from "./navbar";
+
 export function SiteHeader() {
-    return <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">header</header>
+  return (
+    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <Navbar />
+        <div className="flex flex-1 items-center justify-end space-x-2">
+          <nav className="flex items-center">
+            <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+              <div className={cn(buttonVariants({ variant: "ghost" }), "w-10 px-0")}>
+                <Github className="h-4 w-4" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+              <div className={cn(buttonVariants({ variant: "ghost" }), "w-10 px-0")}>
+                <Send className="h-4 w-4" />
+                <span className="sr-only">Telegram</span>
+              </div>
+            </Link>
+
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
 }
