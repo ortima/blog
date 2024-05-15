f4b61467f889f5745f41ee549a9636460cb481af
ortima <account.net@.mail.ru>
2024-05-16 01:45:39 +0300

    icons mobile

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 import { Button } from './ui/button'
-import { Menu, Rss } from 'lucide-react'
+import { Github, Menu, Rss, Send } from 'lucide-react'
 import Link, { LinkProps } from 'next/link'
 import { useRouter } from 'next/navigation'
 import { siteConfig } from '@/config/site'
@@ -30,12 +30,18 @@ const MobileNav = () => {
                     <MobileLink onOpenChange={setOpen} href="/about">
                         About
                     </MobileLink>
-                    <Link target='_blank' rel='noreferrer' href={siteConfig.links.github}>
-                        GitHub
-                    </Link>
-                    <Link target='_blank' rel='noreferrer' href={siteConfig.links.telegram}>
-                        Telegram
-                    </Link>
+                    <div className='flex gap-2 items-center'>
+                        <Link target='_blank' rel='noreferrer' href={siteConfig.links.github}>
+                            GitHub
+                        </Link>
+                        <Github className='h-4 w-4' />
+                    </div>
+                    <div className='flex gap-2 items-center'>
+                        <Link target='_blank' rel='noreferrer' href={siteConfig.links.telegram}>
+                            Telegram
+                        </Link>
+                        <Send className='h-4 w-4' />
+                    </div>
                 </div>
             </SheetContent>
         </Sheet>
