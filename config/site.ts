102917ee30a88a67aefbd9d764010dd5914d8051
ortima <account.net@.mail.ru>
2024-05-19 13:44:24 +0300

    update public URL

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 
 export const siteConfig = {
     name: "Ortima's blog",
-    url: "https://example.com",
+    url: "https://blog-inky-one-34.vercel.app/",
     description: "Next.js 14 blog using Velite, Tailwind CSS and shadcn UI",
     author: "ortima",
     links: {
